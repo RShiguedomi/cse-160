bcfd75bd0f70578b0408ed2ced4bbb9f41e4037d
Rafael Shiguedomi <rshigued@ucsc.edu>
2026-01-20 22:01:30 -0800

    Completed asgn1

--- asgn1/ColoredPoints.js
+++ asgn1/ColoredPoints.js
@@ -29,7 +29,8 @@ function setupWebGL() {
   canvas = document.getElementById('webgl');
 
   // Get the rendering context for WebGL
-  gl = getWebGLContext(canvas);
+  //gl = getWebGLContext(canvas);
+  gl = canvas.getContext("webgl", { preserveDrawingBuffer: true});
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
@@ -65,19 +66,31 @@ function connectVariablesToGLSL() {
   }
 }
 
+const POINT = 0;
+const TRIANGLE = 1;
+const CIRCLE = 2;
+
 let g_selectedColor=[1.0,1.0,1.0,1.0];
 let g_selectedSize=5;
+let g_selectedType=POINT;
+let g_selectedSegments=10;
 // Set up actions for HTML UI elements
 function addActionsForHtmlUI() {
   document.getElementById('green').onclick = function() {g_selectedColor = [0.0, 1.0, 0.0, 1.0]; };
   document.getElementById('red').onclick = function() {g_selectedColor = [1.0, 0.0, 0.0, 1.0]; };
-  //document.getElementById('clearButton').onclick = function() {g_shapesList=[]; renderAllShapes(); };
+  document.getElementById('clearButton').onclick = function() {g_shapesList=[]; renderAllShapes(); };
+
+  document.getElementById('pointButton').onclick = function() {g_selectedType=POINT};
+  document.getElementById('triButton').onclick = function() {g_selectedType=TRIANGLE};
+  document.getElementById('circleButton').onclick = function() {g_selectedType=CIRCLE};
+  document.getElementById('drawPictureButton').onclick = function() {drawMyPicture(); };
 
   document.getElementById('redSlide').addEventListener('mouseup', function() { g_selectedColor[0] = this.value/100; });
   document.getElementById('greenSlide').addEventListener('mouseup', function() { g_selectedColor[1] = this.value/100; });
   document.getElementById('blueSlide').addEventListener('mouseup', function() { g_selectedColor[2] = this.value/100; });
 
   document.getElementById('sizeSlide').addEventListener('mouseup', function() { g_selectedSize = this.value; });
+  document.getElementById('segmentSlide').addEventListener('mouseup', function() { g_selectedSegments = this.value; });
 }
 
 function main() {
@@ -90,7 +103,7 @@ function main() {
 
   // Register function (event handler) to be called on a mouse press
   canvas.onmousedown = click;
-  //canvas.onmousemove = function(ev) { if(ev.buttons == 1) { click(ev) } };
+  canvas.onmousemove = function(ev) { if(ev.buttons == 1) { click(ev) } };
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -101,34 +114,24 @@ function main() {
 
 var g_shapesList = [];
 
-// var g_points = [];  // The array for the position of a mouse press
-// var g_colors = [];  // The array to store the color of a point
-// var g_sizes  = [];  // the array to store the size of a point
 function click(ev) {
   // Extract event click and return it in WebGL coordinates
   let [x, y] = convertCoordinatesEventToGL(ev);
 
   // Create and store new point
-  let point = new Point();
+  let point;
+  if (g_selectedType==POINT){
+    point = new Point();
+  } else if (g_selectedType==TRIANGLE) {
+    point = new Triangle();
+  } else {
+    point = new Circle();
+    point.segment = g_selectedSegments;
+  }
   point.position=[x,y];
   point.color=g_selectedColor.slice();
   point.size=g_selectedSize;
   g_shapesList.push(point);
-
-  // // Store the coordinates to g_points array
-  // g_points.push([x, y]);
-  // // Store color to g_colors array
-  // g_colors.push(g_selectedColor.slice());
-  // // Store size to g_sizes array
-  // g_sizes.push(g_selectedSize);
-  // Store the coordinates to g_points array
-  // if (x >= 0.0 && y >= 0.0) {      // First quadrant
-  //   g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
-  // } else if (x < 0.0 && y < 0.0) { // Third quadrant
-  //   g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
-  // } else {                         // Others
-  //   g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
-  // }
 
   // Draw every shape that is supposed to be in the canvas
   renderAllShapes();
@@ -146,13 +149,152 @@ function convertCoordinatesEventToGL(ev) {
 }
 
 function renderAllShapes() {
+  // Check time at start of this function
+  var startTime = performance.now();
+
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   var len = g_shapesList.length;
   for(var i = 0; i < len; i++) {
-    g_shapesList.render();
+    g_shapesList[i].render();
   }
+
+  var duration = performance.now() - startTime;
+  sendTextToHTML("numdot: " + len + " ms: " + Math.floor(duration) + " fps: " + Math.floor(10000/duration) );
 }
 
+function sendTextToHTML(text, htmlID) {
+  var htmlElm = document.getElementById(htmlID);
+  if (!htmlElm) {
+    console.log("Failed to get " + htmlID + " from HTML");
+    return;
+  }
+  htmlElm.innerHTML = text;
+}
 
+function drawMyPicture() {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  function drawTri(coords, color) {
+    gl.uniform4f(u_FragColor, color[0], color[1], color[2], color[3]);
+    drawTriangle(coords);
+  }
+
+  // Head
+  drawTri([-0.4, 0.3,  0.4, 0.3,  0.4, -0.1], [0.24,0.16,0,1]);
+  drawTri([-0.4, 0.3,  -0.4, -0.1,  0.4, -0.1], [0.24,0.16,0,1]);
+  drawTri([-0.4, 0.3,  -0.35, 0.4,  0.0, 0.3], [0.24,0.16,0,1]);
+  drawTri([-0.35, 0.4,  -0.05, 0.4,  0.0, 0.3], [0.24,0.16,0,1]);
+  drawTri([0.4, 0.3,  0.35, 0.4,  0.0, 0.3], [0.24,0.16,0,1]);
+  drawTri([0.35, 0.4,  0.05, 0.4,  0.0, 0.3], [0.24,0.16,0,1]);
+  drawTri([-0.4, -0.1,  -0.35, -0.2,  0.4, -0.1], [0.24,0.16,0,1]);
+  drawTri([-0.35, -0.2,  0.35, -0.2,  0.4, -0.1], [0.24,0.16,0,1]);
+
+  // Eyes & Mouth
+  drawTri([-0.3, 0.05,  -0.1, 0.05,  -0.1, 0.25], [0,0.14,0.04,1]);
+  drawTri([-0.3, 0.05,  -0.3, 0.25,  -0.1, 0.25], [0,0.14,0.04,1]);
+  drawTri([-0.3, 0.25,  -0.25, 0.3,  -0.1, 0.25], [0,0.14,0.04,1]);
+  drawTri([-0.15, 0.3,  -0.25, 0.3,  -0.1, 0.25], [0,0.14,0.04,1]);
+  drawTri([-0.3, 0.05,  -0.1, 0.05,  -0.25, 0.0], [0,0.14,0.04,1]);
+  drawTri([-0.15, 0.0,  -0.1, 0.05,  -0.25, 0.0], [0,0.14,0.04,1]);
+  drawTri([0.3, 0.05,  0.1, 0.05,  0.1, 0.25], [0,0.14,0.04,1]);
+  drawTri([0.3, 0.05,  0.3, 0.25,  0.1, 0.25], [0,0.14,0.04,1]);
+  drawTri([0.3, 0.25,  0.25, 0.3,  0.1, 0.25], [0,0.14,0.04,1]);
+  drawTri([0.15, 0.3,  0.25, 0.3,  0.1, 0.25], [0,0.14,0.04,1]);
+  drawTri([0.3, 0.05,  0.1, 0.05,  0.25, 0.0], [0,0.14,0.04,1]);
+  drawTri([0.15, 0.0,  0.1, 0.05,  0.25, 0.0], [0,0.14,0.04,1]);
+  drawTri([-0.2, 0.2,  -0.16, 0.23,  -0.125, 0.2], [0.8,0.36,0.36,1]);
+  drawTri([-0.2, 0.2,  -0.16, 0.12,  -0.125, 0.2], [0.8,0.36,0.36,1]);
+  drawTri([-0.2, 0.2,  -0.16, 0.12,  -0.2, 0.15], [0.8,0.36,0.36,1]);
+  drawTri([-0.125, 0.15,  -0.16, 0.12,  -0.125, 0.2], [0.8,0.36,0.36,1]);
+  drawTri([0.2, 0.2,  0.16, 0.23,  0.125, 0.2], [0.8,0.36,0.36,1]);
+  drawTri([0.2, 0.2,  0.16, 0.12,  0.125, 0.2], [0.8,0.36,0.36,1]);
+  drawTri([0.2, 0.2,  0.16, 0.12,  0.2, 0.15], [0.8,0.36,0.36,1]);
+  drawTri([0.125, 0.15,  0.16, 0.12,  0.125, 0.2], [0.8,0.36,0.36,1]);
+
+  drawTri([-0.05, -0.1,  0.0, -0.05,  0.05, -0.1], [0,0.14,0.04,1]);
+  drawTri([-0.05, -0.1,  0.0, -0.15,  0.05, -0.1], [0,0.14,0.04,1]);
+
+  // Horns
+  drawTri([-0.4, 0.0,  -0.4, 0.2,  -0.7, 0.0], [0.24,0.16,0,1]);
+  drawTri([-0.7, 0.2,  -0.4, 0.2,  -0.7, 0.0], [0.24,0.16,0,1]);
+  drawTri([-0.7, 0.2,  -0.8, 0.1,  -0.7, 0.0], [0.24,0.16,0,1]);
+  drawTri([-0.7, 0.2,  -0.65, 0.3,  -0.6, 0.2], [0.24,0.16,0,1]);
+  drawTri([-0.7, 0.2,  -0.65, 0.3,  -0.75, 0.3], [0.24,0.16,0,1]);
+  drawTri([-0.7, 0.2,  -0.8, 0.1,  -0.8, 0.4], [0.24,0.16,0,1]);
+  drawTri([-0.8, 0.2,  -0.9, 0.25,  -0.9, 0.4], [0.24,0.16,0,1]);
+  drawTri([-0.8, 0.2,  -0.8, 0.6,  -0.9, 0.4], [0.24,0.16,0,1]);
+
+  drawTri([0.4, 0.0,  0.4, 0.2,  0.7, 0.0], [0.24,0.16,0,1]);
+  drawTri([0.7, 0.2,  0.4, 0.2,  0.7, 0.0], [0.24,0.16,0,1]);
+  drawTri([0.7, 0.2,  0.8, 0.1,  0.7, 0.0], [0.24,0.16,0,1]);
+  drawTri([0.7, 0.2,  0.65, 0.3,  0.6, 0.2], [0.24,0.16,0,1]);
+  drawTri([0.7, 0.2,  0.65, 0.3,  0.75, 0.3], [0.24,0.16,0,1]);
+  drawTri([0.7, 0.2,  0.8, 0.1,  0.8, 0.4], [0.24,0.16,0,1]);
+  drawTri([0.8, 0.2,  0.9, 0.25,  0.9, 0.4], [0.24,0.16,0,1]);
+  drawTri([0.8, 0.2,  0.8, 0.6,  0.9, 0.4], [0.24,0.16,0,1]);
+
+  // Leaves
+  drawTri([0.8, 0.4,  0.7, 0.4,  0.6, 0.45], [0.2,0.29,0.13,1]);
+  drawTri([0.8, 0.4,  0.7, 0.45,  0.6, 0.45], [0.2,0.29,0.13,1]);
+  drawTri([-0.8, 0.4,  -0.7, 0.4,  -0.6, 0.45], [0.2,0.29,0.13,1]);
+  drawTri([-0.8, 0.4,  -0.7, 0.45,  -0.6, 0.45], [0.2,0.29,0.13,1]);
+  drawTri([0.8, 0.15,  0.9, 0.2,  1, 0.2], [0.2,0.29,0.13,1]);
+  drawTri([0.8, 0.15,  0.9, 0.15,  1, 0.2], [0.2,0.29,0.13,1]);
+  drawTri([-0.8, 0.15,  -0.9, 0.2,  -1, 0.2], [0.2,0.29,0.13,1]);
+  drawTri([-0.8, 0.15,  -0.9, 0.15,  -1, 0.2], [0.2,0.29,0.13,1]);
+
+
+  // Initials
+  drawTri([-0.2, -0.4,  -0.2, -0.6,  -0.1, -0.6], [0.2,0.24,0.0,1]);
+  drawTri([-0.2, -0.4,  -0.1, -0.35,  -0.1, -0.6], [0.2,0.24,0.0,1]);
+  drawTri([0.0, -0.45,  -0.1, -0.35,  -0.1, -0.55], [0.2,0.24,0.0,1]);
+  drawTri([-0.05, -0.5,  -0.05, -0.6,  -0.1, -0.55], [0.2,0.24,0.0,1]);
+  drawTri([-0.05, -0.5,  -0.05, -0.6,  0.0, -0.55], [0.2,0.24,0.0,1]);
+  drawTri([0.0, -0.6,  -0.05, -0.6,  0.0, -0.55], [0.2,0.24,0.0,1]);
+
+  drawTri([0.1, -0.35,  0.1, -0.45,  0.0, -0.4], [0.2,0.24,0.0,1]);
+  drawTri([0.1, -0.35,  0.1, -0.45,  0.2, -0.4], [0.2,0.24,0.0,1]);
+  drawTri([0.1, -0.5,  0.1, -0.6,  0.0, -0.55], [0.2,0.24,0.0,1]);
+  drawTri([0.1, -0.5,  0.1, -0.6,  0.2, -0.55], [0.2,0.24,0.0,1]);
+  drawTri([0.0, -0.55,  0.0, -0.5,  0.1, -0.5], [0.2,0.24,0.0,1]);
+  drawTri([0.2, -0.45,  0.2, -0.4,  0.1, -0.45], [0.2,0.24,0.0,1]);
+  drawTri([0.0, -0.4,  0.0, -0.45,  0.2, -0.5], [0.2,0.24,0.0,1]);
+  drawTri([0.2, -0.55,  0.0, -0.45,  0.2, -0.5], [0.2,0.24,0.0,1]);
+
+  // Head & Body Wisps
+  drawTri([-0.05, 0.4,  0.0, 0.3,  0.05, 0.4], [0,0.14,0.04,1]);
+  drawTri([-0.05, 0.4,  0.1, 0.4,  0.1, 0.5], [0,0.14,0.04,1]);
+  drawTri([0.2, 0.4,  0.1, 0.4,  0.1, 0.5], [0,0.14,0.04,1]);
+  drawTri([-0.05, 0.4,  0.05, 0.6,  0.1, 0.5], [0,0.14,0.04,1]);
+  drawTri([-0.05, 0.4,  -0.2, 0.4,  0.0, 0.5], [0,0.14,0.04,1]);
+  drawTri([-0.2, 0.5,  -0.2, 0.4,  0.0, 0.5], [0,0.14,0.04,1]);
+  drawTri([-0.2, 0.5,  0.0, 0.6,  0.0, 0.5], [0,0.14,0.04,1]);
+  drawTri([-0.2, 0.5,  0.0, 0.6,  -0.15, 0.6], [0,0.14,0.04,1]);
+  drawTri([-0.05, 0.7,  0.0, 0.6,  -0.15, 0.6], [0,0.14,0.04,1]);
+  drawTri([0.0, 0.5,  0.1, 0.7,  0.0, 0.8], [0,0.14,0.04,1]);
+  drawTri([0.0, 0.6,  -0.1, 0.8,  0.0, 0.9], [0,0.14,0.04,1]);
+
+  drawTri([-0.1, -0.2,  -0.2, -0.4,  -0.1, -0.35], [0,0.14,0.04,1]);
+  drawTri([-0.1, -0.2,  0.0, -0.4,  -0.1, -0.35], [0,0.14,0.04,1]);
+  drawTri([-0.1, -0.2,  0.0, -0.4,  0.1, -0.2], [0,0.14,0.04,1]);
+  drawTri([0.1, -0.2,  0.2, -0.4,  0.1, -0.35], [0,0.14,0.04,1]);
+  drawTri([0.1, -0.2,  0.0, -0.4,  0.1, -0.35], [0,0.14,0.04,1]);
+  drawTri([0.0, -0.45,  0.0, -0.4,  -0.1, -0.35], [0,0.14,0.04,1]);
+  drawTri([0.0, -0.45,  0.0, -0.55,  -0.05, -0.5], [0,0.14,0.04,1]);
+  drawTri([0.0, -0.45,  0.1, -0.5,  0.0, -0.5], [0,0.14,0.04,1]);
+  drawTri([0.2, -0.45,  0.1, -0.45,  0.2, -0.5], [0,0.14,0.04,1]);
+  drawTri([-0.1, -0.55,  -0.1, -0.6,  -0.05, -0.6], [0,0.14,0.04,1]);
+  drawTri([0.0, -0.55,  0.1, -0.6,  0.0, -0.6], [0,0.14,0.04,1]);
+  drawTri([0.2, -0.55,  0.1, -0.6,  0.2, -0.6], [0,0.14,0.04,1]);
+  drawTri([-0.1, -0.4,  -0.1, -0.5,  -0.05, -0.45], [0,0.14,0.04,1]);
+  drawTri([0.0, -0.8,  0.2, -0.6,  -0.2, -0.6], [0,0.14,0.04,1]);
+  drawTri([0.0, -0.8,  -0.15, -0.7,  -0.2, -0.6], [0,0.14,0.04,1]);
+  drawTri([0.0, -0.8,  0.2, -0.6,  0.2, -0.8], [0,0.14,0.04,1]);
+  drawTri([0.2, -0.8,  0.2, -0.6,  0.4, -0.7], [0,0.14,0.04,1]);
+  drawTri([0.45, -0.6,  0.2, -0.6,  0.4, -0.7], [0,0.14,0.04,1]);
+  drawTri([0.45, -0.6,  0.2, -0.6,  0.5, -0.5], [0,0.14,0.04,1]);
+  drawTri([0.45, -0.6,  0.6, -0.7,  0.5, -0.5], [0,0.14,0.04,1]);
+  drawTri([-0.2, -0.4,  -0.2, -0.5,  -0.35, -0.4], [0,0.14,0.04,1]);
+  drawTri([0.2, -0.4,  0.2, -0.5,  0.35, -0.4], [0,0.14,0.04,1]);
+}
--- asgn1/Point.js
+++ asgn1/Point.js
@@ -1,16 +1,19 @@
 class Point{
-  constructor() {
+  constructor(){
     this.type='point';
     this.position = [0.0,0.0,0.0];
     this.color = [1.0,1.0,1.0,1.0];
     this.size = 5.0; 
   }
-
+  // Render this shape
   render() {
     var xy = this.position;
     var rgba = this.color;
     var size = this.size;
 
+    // Quit using the buffer to send the attribute
+    gl.disableVertexAttribArray(a_Position);
+    //gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([ xy[0], xy[1]]), gl.DYNAMIC_DRAW);
     // Pass the position of a point to a_Position variable
     gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
     // Pass the color of a point to u_FragColor variable
